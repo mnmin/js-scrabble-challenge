98022fe0ed582c4747663945a71274216f0748a3
Michele M <mnmin@gmail.com>
2022-05-25 18:03:54 +0100

    Scrabble completed

--- src/scrabble.js
+++ src/scrabble.js
@@ -2,14 +2,13 @@
 // 2 - Create a function for initial score = 0 for no words/letters
 // 3 - Create a fuction where you input a letter/word and it calucates, a score and reassign this score to score value.
 
-class Scrabble {
-  // Write your implementation here
+// Restart
+//1 create a class for Scrabble where you input a word and the output is a score for that word.
+// 1 
 
-}
-// Create an obbect where keys equal to letters and their values for Scrabble
-const ScrabbleScore = {
+const scrabbleScore = {
   a: 1,
-  b: 2,
+  b: 3,
   c: 3,
   d: 2,
   e: 1,
@@ -35,6 +34,49 @@ const ScrabbleScore = {
   y: 4,
   z: 10,
 }
+
+
+class Scrabble {
+  constructor (word) {
+    this.word = word
+    this.totalScore = 0 
+  }
+  
+  score () {
+    if (this.word === null) {
+      return 0
+    }
+    // Loop through each letter of the word. 
+    for (let i = 0; i < this.word.length; i++) {  
+      // let i = 0 starts the for loop at index 0.
+      // i smaller than this.word.length because if larger, loop stops.
+    //  console.log(i, this.word.charAt(i))
+      // prints character at index i
+      const currentLetter = this.word.charAt(i).toLowerCase()
+      // new variable, this is needed to store printed character from loop
+     // console.log(scrabbleScore[currentLetter])
+      // prints scrabbleScore of currentLetter. 
+      const letterScore = scrabbleScore[currentLetter]
+      if (letterScore === undefined) {
+         continue
+      }
+      // new variable to store scrabble score for currentletter index score
+      this.totalScore = this.totalScore + letterScore
+      // add letter score to total score
+    }
+    
+    
+    // When the loop passes for each letter I need to get the score for that letter through scrabbleScore
+    // I need to add the letter score to the total score to this.totalScore.
+    return this.totalScore
+  }
+}
+let scrabble = new Scrabble ('')
+//console.log(scrabble.word)
+//console.log(scrabble.totalScore)
+//console.log(scrabble.score())
+// Create an obbect where keys equal to letters and their values for Scrabble
+
 
 
 module.exports = Scrabble

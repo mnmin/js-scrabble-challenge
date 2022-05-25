2f7ea97015aebcd8fff39259c205ea1cd965b23c
Michele M <mnmin@gmail.com>
2022-05-25 12:10:54 +0100

    Scrabble Object

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,40 @@
+// 1 - Create object with keys equal to letters and their values
+// 2 - Create a function for initial score = 0 for no words/letters
+// 3 - Create a fuction where you input a letter/word and it calucates, a score and reassign this score to score value.
+
 class Scrabble {
   // Write your implementation here
 
 }
+// Create an obbect where keys equal to letters and their values for Scrabble
+const ScrabbleScore = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 2,
+  e: 1,
+  f: 4,
+  g: 2,
+  h: 4,
+  i: 1,
+  j: 8,
+  k: 5,
+  l: 1,
+  m: 3,
+  n: 1,
+  o: 1,
+  p: 3,
+  q: 10,
+  r: 1,
+  s: 1,
+  t: 1,
+  u: 1,
+  v: 4,
+  w: 4,
+  x: 8,
+  y: 4,
+  z: 10,
+}
+
 
 module.exports = Scrabble
